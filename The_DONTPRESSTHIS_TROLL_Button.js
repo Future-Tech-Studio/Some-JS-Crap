4597377759b3bfaedc6b8d3a27403b9d4f187057
articuno032 <articuno032@gmail.com>
2017-01-11 09:11:06 -0500

    Update The_DONTPRESSTHIS_TROLL_Button.js

--- The_DONTPRESSTHIS_TROLL_Button.js
+++ The_DONTPRESSTHIS_TROLL_Button.js
@@ -6,7 +6,7 @@ x.appendChild(d);
 // Setting the Appended button's text to "DON'T CLICK ME!"
 d.innerHTML = "DON'T CLICK ME!";
 // making an "onclick" function to activate the button
-d.onclick = function() {otherfunc()};
+d.onclick = function() {otherfunc();};
 // calling the function and maikng the page turn into this: "I Told You 'DON'T CLICK ME!'", and setting the background to white.
 function otherfunc() {
   x.innerHTML = "I told you 'DON'T CLICK ME!'";
